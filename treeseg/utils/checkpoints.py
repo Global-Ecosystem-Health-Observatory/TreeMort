29e8970bcfd0323c0f06f2f968b7f1fee94a12dd
Anis Rahman <rahmanan@puhti-login15.bullx>
2024-07-10 00:05:41 +0300

    Fixed best checkpoint

--- treeseg/utils/checkpoints.py
+++ treeseg/utils/checkpoints.py
@@ -5,13 +5,12 @@ import tensorflow as tf
 
 
 def get_checkpoint(model_weights, output_dir):
-    checkpoint_dir = os.path.join(output_dir, "Checkpoints")
-
     checkpoint = None
+    
     if model_weights == "best":
-        checkpoint = os.path.join(checkpoint_dir, "best.weights.h5")
+        checkpoint = os.path.join(output_dir, "best.weights.h5")
 
     elif model_weights == "latest":
-        checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+        checkpoint = tf.train.latest_checkpoint(os.path.join(output_dir, "Checkpoints"))
 
     return checkpoint
