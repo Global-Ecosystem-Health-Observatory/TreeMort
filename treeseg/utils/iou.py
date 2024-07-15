db55899a0df044e24fa976509a0bc65ff798cd6e
Anis Rahman <anisr@HP4CX34M7C.local>
2024-07-15 11:49:19 +0300

    Added device support

--- treeseg/utils/iou.py
+++ treeseg/utils/iou.py
@@ -1,8 +1,7 @@
+import torch
 import numpy as np
 from tqdm import tqdm
 from scipy import ndimage
-import torch
-
 
 class IOUCallback:
     def __init__(self, model, dataset, num_samples, batch_size, threshold):
@@ -11,84 +10,80 @@ class IOUCallback:
         self.num_samples = num_samples
         self.batch_size = batch_size
         self.threshold = threshold
+        self.device = next(model.parameters()).device  # Get the device of the model
 
     def evaluate(self):
-        self.model.eval()  # Set model to evaluation mode
+        self.model.eval()  # Set the model to evaluation mode
         pixel_ious = []
         tree_ious = []
 
         total_batches = self.num_samples // self.batch_size
 
         with tqdm(total=total_batches, desc="Evaluating") as pbar:
-            for images, labels in self.dataset:
-                if torch.cuda.is_available():
-                    images = images.cuda()
-                    labels = labels.cuda()
-                with torch.no_grad():
+
+            with torch.no_grad():
+                for images, labels in self.dataset:
+                    images = images.to(self.device)
+                    labels = labels.to(self.device)
+
                     predictions = self.model(images)
+                    
+                    predictions = predictions.cpu().numpy()
+                    labels = labels.cpu().numpy()
 
-                predictions = predictions.cpu().numpy()
-                labels = labels.cpu().numpy()
+                    for i in range(len(predictions)):
+                        y_pred = predictions[i]
+                        y_true = labels[i]
 
-                for i in range(len(predictions)):
-                    y_pred = predictions[i]
-                    y_true = labels[i]
+                        y_pred_binary = np.squeeze(y_pred > self.threshold)
+                        y_true_binary = np.squeeze(y_true > self.threshold)
 
-                    y_pred_binary = np.squeeze(y_pred[0] > self.threshold)
-                    y_true_binary = np.squeeze(y_true[0] > self.threshold)
+                        # Pixel-wise IoU
+                        tp_pixels = np.sum(np.logical_and(y_pred_binary, y_true_binary))
+                        fp_pixels = np.sum(np.logical_and(y_pred_binary, np.logical_not(y_true_binary)))
+                        fn_pixels = np.sum(np.logical_and(np.logical_not(y_pred_binary), y_true_binary))
 
-                    # Pixel-wise IoU
-                    tp_pixels = np.sum(np.logical_and(y_pred_binary, y_true_binary))
-                    fp_pixels = np.sum(
-                        np.logical_and(y_pred_binary, np.logical_not(y_true_binary))
-                    )
-                    fn_pixels = np.sum(
-                        np.logical_and(np.logical_not(y_pred_binary), y_true_binary)
-                    )
-
-                    if (tp_pixels + fp_pixels + fn_pixels) == 0:
-                        iou_pixels = 1.0 if np.sum(y_true_binary) == 0 else 0.0
-                    else:
-                        iou_pixels = tp_pixels / (tp_pixels + fp_pixels + fn_pixels)
-                    pixel_ious.append(iou_pixels)
-
-                    # Tree-wise IoU
-                    labeled_array_pred, num_features_pred = ndimage.label(y_pred_binary)
-                    labeled_array_true, num_features_true = ndimage.label(y_true_binary)
-                    predicted_contour_numbers = np.unique(labeled_array_pred)
-                    true_contour_numbers = np.unique(labeled_array_true)
-
-                    tp_trees, fp_trees, fn_trees = 0, 0, 0
-
-                    for predicted_contour_number in predicted_contour_numbers:
-                        if predicted_contour_number == 0:
-                            continue
-                        predicted_contour_mask = (
-                            labeled_array_pred == predicted_contour_number
-                        )
-                        prediction_exists = y_true_binary[predicted_contour_mask].any()
-                        if prediction_exists:
-                            tp_trees += 1
+                        if (tp_pixels + fp_pixels + fn_pixels) == 0:
+                            iou_pixels = 1.0 if np.sum(y_true_binary) == 0 else 0.0
                         else:
-                            fp_trees += 1
+                            iou_pixels = tp_pixels / (tp_pixels + fp_pixels + fn_pixels)
+                        pixel_ious.append(iou_pixels)
 
-                    for true_contour_number in true_contour_numbers:
-                        if true_contour_number == 0:
-                            continue
-                        true_contour_mask = labeled_array_true == true_contour_number
-                        prediction_exists = y_pred_binary[true_contour_mask].any()
-                        if not prediction_exists:
-                            fn_trees += 1
+                        # Tree-wise IoU
+                        labeled_array_pred, num_features_pred = ndimage.label(y_pred_binary)
+                        labeled_array_true, num_features_true = ndimage.label(y_true_binary)
+                        predicted_contour_numbers = np.unique(labeled_array_pred)
+                        true_contour_numbers = np.unique(labeled_array_true)
 
-                    if (tp_trees + fp_trees + fn_trees) == 0:
-                        iou_trees = 1.0 if num_features_true == 0 else 0.0
-                    else:
-                        iou_trees = tp_trees / (tp_trees + fp_trees + fn_trees)
-                    tree_ious.append(iou_trees)
+                        tp_trees, fp_trees, fn_trees = 0, 0, 0
 
-                # Update the progress bar
-                pbar.update(1)
-                pbar.set_postfix(iterations_left=total_batches - pbar.n)
+                        for predicted_contour_number in predicted_contour_numbers:
+                            if predicted_contour_number == 0:
+                                continue
+                            predicted_contour_mask = (labeled_array_pred == predicted_contour_number)
+                            prediction_exists = y_true_binary[predicted_contour_mask].any()
+                            if prediction_exists:
+                                tp_trees += 1
+                            else:
+                                fp_trees += 1
+
+                        for true_contour_number in true_contour_numbers:
+                            if true_contour_number == 0:
+                                continue
+                            true_contour_mask = (labeled_array_true == true_contour_number)
+                            prediction_exists = y_pred_binary[true_contour_mask].any()
+                            if not prediction_exists:
+                                fn_trees += 1
+
+                        if (tp_trees + fp_trees + fn_trees) == 0:
+                            iou_trees = 1.0 if num_features_true == 0 else 0.0
+                        else:
+                            iou_trees = tp_trees / (tp_trees + fp_trees + fn_trees)
+                        tree_ious.append(iou_trees)
+
+                    # Update the progress bar
+                    pbar.update(1)
+                    pbar.set_postfix(iterations_left=total_batches - pbar.n)
 
         mean_iou_pixels = np.mean([iou for iou in pixel_ious if iou != -1])
         mean_iou_trees = np.mean([iou for iou in tree_ious if iou != -1])
