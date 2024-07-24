b1244894726a0e4363037b04cfbbbc0452e91fac
Anis Rahman <anisr@HP4CX34M7C.local>
2024-07-25 00:29:51 +0300

    Bug fix BEiT

--- treemort/modeling/trainer.py
+++ treemort/modeling/trainer.py
@@ -38,21 +38,21 @@ def trainer(
             if conf.model == "maskformer":
                 outputs = model(images)
         
-                target_sizes = [(image.shape[1], image.shape[2]) for image in images]
+                target_sizes = [(label.shape[1], label.shape[2]) for label in labels]
                 predictions = image_processor.post_process_semantic_segmentation(outputs, target_sizes=target_sizes)
                 predictions = torch.stack([prediction.unsqueeze(0) for prediction in predictions], dim=0).float().to(device)
             
             elif conf.model == "detr":
                 outputs = model(images)
 
-                target_sizes = [(image.shape[1], image.shape[2]) for image in images]
+                target_sizes = [(label.shape[1], label.shape[2]) for label in labels]
                 predictions = image_processor.post_process_panoptic_segmentation(outputs, target_sizes=target_sizes)
                 predictions = torch.stack([prediction['segmentation'].unsqueeze(0).to(device) for prediction in predictions], dim=0).float()
 
             elif conf.model == "beit":
                 outputs = model(images)
 
-                target_sizes = [(image.shape[1], image.shape[2]) for image in images]
+                target_sizes = [(label.shape[1], label.shape[2]) for label in labels]
                 predictions = image_processor.post_process_semantic_segmentation(outputs, target_sizes=target_sizes)
                 predictions = torch.stack([prediction.unsqueeze(0).to(device) for prediction in predictions], dim=0).float()
 
