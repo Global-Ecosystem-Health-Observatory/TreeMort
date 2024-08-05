b9cb73a58bbce0dc41de65c13cf289bac1df7f0d
Anis Rahman <anisr@HP4CX34M7C.local>
2024-08-05 19:48:26 +0300

    Updated dataset creator

--- dataset/creator.py
+++ dataset/creator.py
@@ -1,15 +1,10 @@
 import os
-import cv2
-import json
 import shutil
 import random
+import logging
 import argparse
-import rasterio
 
 import numpy as np
-
-from tqdm import tqdm
-from matplotlib import pyplot as plt
 
 from preprocessutils import (
     get_image_and_polygons_reorder,
@@ -17,17 +12,23 @@ from preprocessutils import (
     segmap_to_topo,
 )
 
-
+# Configuration
 square_save_size = 320
 
 # nir_r_g_b_order only for highres sat images (otherwise set to None)
-nir_r_g_b_order = [3, 2, 1, 0]  # [3,0,1,2] # SET UP NIR-R-G-B order for example: PHR [3,0,1,2] -- alueX SS_PS [3,2,1,0]
+# SET UP NIR-R-G-B order for example: PHR [3,0,1,2] -- alueX SS_PS [3,2,1,0]
+nir_r_g_b_order = [3,2,1,0]
 
 normalize_imagewise = False  # each image_crop normalized separately to 0...255
 normalize_channelwise = True  # each channel normalized separately to 0...255
 
 
-""" CREATE the samples to temp folder"""
+# Set up logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+def create_directory(directory_path):
+    os.makedirs(directory_path, exist_ok=True)
 
 
 def create_dataset(project_folder, no_of_samples=None):
@@ -35,26 +36,23 @@ def create_dataset(project_folder, no_of_samples=None):
     output_labels_folder = os.path.join(project_folder, "Temp", "Labels")
 
     for folder_path in [output_images_folder, output_labels_folder]:
-        os.makedirs(folder_path, exist_ok=True)
+        create_directory(folder_path)
 
-    if no_of_samples is None:
-        filut = os.listdir(image_folder)
-    else:
-        filut = os.listdir(image_folder)[:no_of_samples]
+    image_folder = os.path.join(project_folder, "4band_25cm")
+    label_folder = os.path.join(project_folder, "Geojsons")
+
+    file_list = os.listdir(image_folder)[:no_of_samples] if no_of_samples else os.listdir(image_folder)
 
     total_files_in_area, total_annotations_in_area, no_of_samples = 0, 0, 0
 
-    for filu in filut:
+    for file in file_list:
         try:
-            if filu.endswith("jp2") or filu.endswith("tif") or filu.endswith("tiff"):
+            if file.endswith(("jp2", "tif", "tiff")):
                 total_files_in_area += 1
-                image_filepath = os.path.join(image_folder, filu)
-                geojson_filepath = os.path.join(
-                    label_folder,
-                    filu.replace(".jp2", ".geojson")
-                    .replace(".tiff", ".geojson")
-                    .replace(".tif", ".geojson"),
-                )
+
+                image_filepath = os.path.join(image_folder, file)
+                geojson_filepath = os.path.join(label_folder, file.rsplit('.', 1)[0] + ".geojson")
+
                 if nir_r_g_b_order is not None:
                     exim_np, adjusted_polygons = get_image_and_polygons_reorder(
                         image_filepath,
@@ -70,48 +68,45 @@ def create_dataset(project_folder, no_of_samples=None):
                         normalize_channelwise,
                         normalize_imagewise,
                     )
+
                 total_annotations_in_area += len(adjusted_polygons)
                 topolabel = segmap_to_topo(exim_np, adjusted_polygons)
+
                 # save images as close to 400 px images as evenly possible
-                orig_h = exim_np.shape[0]
-                orig_w = exim_np.shape[1]
+                orig_h, orig_w = exim_np.shape[:2]
+
                 divider_h = np.ceil(orig_h / square_save_size)
                 divider_w = np.ceil(orig_w / square_save_size)
+
                 sample_h = int(np.floor(orig_h / divider_h))
                 sample_w = int(np.floor(orig_w / divider_w))
-                filestub = os.path.split(image_filepath)[1].split(".")[0]
+
+                file_stub = os.path.split(image_filepath)[1].split(".")[0]
+
                 # split the image to smaller samples
                 for x_tile in range(int(divider_w)):
                     for y_tile in range(int(divider_h)):
-                        start_x = x_tile * sample_w
-                        end_x = start_x + sample_w
-                        start_y = y_tile * sample_h
-                        end_y = start_y + sample_h
+                        start_x, end_x = x_tile * sample_w, (x_tile + 1) * sample_w
+                        start_y, end_y = y_tile * sample_h, (y_tile + 1) * sample_h
+
                         image_clip = exim_np[start_y:end_y, start_x:end_x, :]
                         topo_clip = topolabel[start_y:end_y, start_x:end_x]
-                        # if np.max(topo_clip>0):
-                        res_image_path = os.path.join(
-                            output_images_folder,
-                            (filestub + "_" + str(x_tile) + "_" + str(y_tile) + ".npy"),
-                        )
-                        res_label_path = os.path.join(
-                            output_labels_folder,
-                            (filestub + "_" + str(x_tile) + "_" + str(y_tile) + ".npy"),
-                        )
+
+                        res_image_path = os.path.join(output_images_folder, f"{file_stub}_{x_tile}_{y_tile}.npy")
+                        res_label_path = os.path.join(output_labels_folder, f"{file_stub}_{x_tile}_{y_tile}.npy")
+
                         np.save(res_image_path, image_clip)
                         np.save(res_label_path, topo_clip)
                         no_of_samples += 1
-        except:
-            print(f"failed to process {filu}")
 
-    print(f"{total_files_in_area}  files")
-    print(f"{total_annotations_in_area} polygons")
-    print(f"{no_of_samples} sample-squares")
+        except Exception as e:
+            logging.error(f"Failed to process {file}: {e}")
+
+    logging.info(f"{total_files_in_area} files processed")
+    logging.info(f"{total_annotations_in_area} polygons found")
+    logging.info(f"{no_of_samples} sample-squares created")
 
     return output_images_folder, output_labels_folder
-
-
-""" Split Train / Test data """
 
 
 def split_dataset(
@@ -119,8 +114,8 @@ def split_dataset(
 ):
     train_images_folder = os.path.join(project_folder, "Train", "Images")
     train_labels_folder = os.path.join(project_folder, "Train", "Labels")
-    test_images_folder = os.path.join(project_folder, "Test", "Images")
-    test_labels_folder = os.path.join(project_folder, "Test", "Labels")
+    test_images_folder  = os.path.join(project_folder, "Test", "Images")
+    test_labels_folder  = os.path.join(project_folder, "Test", "Labels")
 
     for folder_path in [
         train_images_folder,
@@ -128,42 +123,37 @@ def split_dataset(
         test_images_folder,
         test_labels_folder,
     ]:
-        os.makedirs(folder_path, exist_ok=True)
+        create_directory(folder_path)
 
     all_files = os.listdir(output_images_folder)
-    # move 10% to test others to train
-    for filu in all_files:
-        if random.randint(1, 10) == split_ratio:  # 10% test set
-            shutil.move(
-                os.path.join(output_images_folder, filu),
-                os.path.join(test_images_folder, filu),
-            )
-            shutil.move(
-                os.path.join(output_labels_folder, filu),
-                os.path.join(test_labels_folder, filu),
-            )
-        else:
-            shutil.move(
-                os.path.join(output_images_folder, filu),
-                os.path.join(train_images_folder, filu),
-            )
-            shutil.move(
-                os.path.join(output_labels_folder, filu),
-                os.path.join(train_labels_folder, filu),
-            )
 
-    print(f"Created Train and Test folders")
-    print(f"Train Images: {train_images_folder}")
-    print(f"Train Labels: {train_labels_folder}")
-    print(f"Test Images: {test_images_folder}")
-    print(f"Test Labels: {test_labels_folder}")
+    for file in all_files:
+        src_image_path = os.path.join(output_images_folder, file)
+        src_label_path = os.path.join(output_labels_folder, file)
+
+        if random.randint(1, 10) == split_ratio:  # 10% test set
+            dest_image_path = os.path.join(test_images_folder, file)
+            dest_label_path = os.path.join(test_labels_folder, file)
+
+        else:
+            dest_image_path = os.path.join(train_images_folder, file)
+            dest_label_path = os.path.join(train_labels_folder, file)
+
+        shutil.move(src_image_path, dest_image_path)
+        shutil.move(src_label_path, dest_label_path)
+
+    logging.info("Created Train and Test folders")
+    logging.info(f"Train Images: {train_images_folder}")
+    logging.info(f"Train Labels: {train_labels_folder}")
+    logging.info(f"Test Images: {test_images_folder}")
+    logging.info(f"Test Labels: {test_labels_folder}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Aerial images dataset creation.")
-    parser.add_argument("data_folder",      type=str,               help="Path to the raw Aerial Image data")
-    parser.add_argument("--no-of-samples",  type=int, default=None, help="If set N>1, creates samples from N Aerial Images, for testing purposes only")
-    
+    parser.add_argument("data_folder",     type=str,               help="Path to the raw Aerial Image data")
+    parser.add_argument("--no-of-samples", type=int, default=None, help="If set N>1, creates samples from N Aerial Images, for testing purposes only")
+
     args = parser.parse_args()
 
     print(args)
@@ -176,5 +166,6 @@ if __name__ == "__main__":
     output_images_folder, output_labels_folder = create_dataset(data_folder, args.no_of_samples)
     split_dataset(data_folder, output_images_folder, output_labels_folder)
 
-    print("Removing Temp folder")
+    logging.info("Removing Temp folder")
     shutil.rmtree(os.path.join(data_folder, "Temp"))
+
