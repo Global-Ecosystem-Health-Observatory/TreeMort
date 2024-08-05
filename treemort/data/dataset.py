6bdfd507eed672eabbf711642bafc9c6583cd18a
Anis Rahman <anisr@HP4CX34M7C.local>
2024-08-05 11:19:39 +0300

    minor cleanup

--- treemort/data/dataset.py
+++ treemort/data/dataset.py
@@ -1,5 +1,3 @@
-# Code borrowed from https://github.com/NielsRogge/Transformers-Tutorials/blob/master/DINOv2/Train_a_linear_classifier_on_top_of_DINOv2_for_semantic_segmentation.ipynb
-
 import os
 import torch
 
