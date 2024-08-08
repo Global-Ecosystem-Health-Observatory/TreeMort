fb8bb5f8dd228ec09642c6e92ccede800d30002e
Anis Rahman <anisr@HP4CX34M7C.local>
2024-08-08 10:47:15 +0300

    Fixed batch file

--- scripts/run_creator.sh
+++ scripts/run_creator.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=treemort-creator
 #SBATCH --account=project_2008436
-#SBATCH --output=stdout/%A_%a
+#SBATCH --output=output/stdout/%A_%a
 #SBATCH --ntasks=1 --cpus-per-task=6
 #SBATCH --time=02:00:00 --partition=small
 #SBATCH --mem-per-cpu=4000
 
 module load python-data
+source venv/bin/activate
 
 srun python3 ./dataset/creator.py ./configs/AerialImageModel_ITD.txt
