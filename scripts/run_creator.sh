38525bdca758222467e18dced44ea0090d2f533d
Anis Rahman <anisr@HP4CX34M7C.local>
2024-08-10 22:05:33 +0300

    Increased resources for creator

--- scripts/run_creator.sh
+++ scripts/run_creator.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=treemort-creator
 #SBATCH --account=project_2008436
 #SBATCH --output=output/stdout/%A_%a
-#SBATCH --ntasks=1 --cpus-per-task=8
-#SBATCH --time=02:00:00 --partition=small
+#SBATCH --ntasks=1 --cpus-per-task=6
+#SBATCH --time=05:00:00 --partition=small
 #SBATCH --mem-per-cpu=6000
 
 module load python-data
