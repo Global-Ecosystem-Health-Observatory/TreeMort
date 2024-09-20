10aba14b66d0b6170fe37d58b0531bb38523cdc5
Anis Rahman <anisr@HP4CX34M7C.local>
2024-09-20 14:54:16 +0300

    Minor fix: Inference script, module load

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -9,8 +9,6 @@
 #SBATCH --partition=small
 #SBATCH --mem-per-cpu=6000
 
-module load pytorch/2.3
-
 VENV_PATH="/projappl/project_2004205/rahmanan/venv"
 
 if [ -d "$VENV_PATH" ]; then
@@ -19,6 +17,8 @@ else
     echo "[ERROR] Virtual environment not found at $VENV_PATH"
     exit 1
 fi
+
+module load pytorch/2.3
 
 DATA_PATH="/scratch/project_2008436/rahmanan/dead_trees/Finland/RGBNIR/25cm"
 CONFIG_PATH="/users/rahmanan/TreeMort/configs/Finland_RGBNIR_25cm_inference.txt"
