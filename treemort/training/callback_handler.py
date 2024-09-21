30dbeaf889c9f7f4a5644a2cef3586d3a1ab3570
Anis Rahman <anisr@HP4CX34M7C.local>
2024-09-21 22:41:01 +0300

    Removed remaining info prints

--- treemort/training/callback_handler.py
+++ treemort/training/callback_handler.py
@@ -1,16 +1,25 @@
+import logging
+
 from treemort.utils.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 
 def handle_callbacks(callbacks, epoch, model, optimizer, val_loss):
     for callback in callbacks:
         if isinstance(callback, ModelCheckpoint):
-            print("[INFO] Invoking ModelCheckpoint callback.")
+            logger.info("Invoking ModelCheckpoint callback.")
             callback(epoch + 1, model, optimizer, val_loss)
 
         elif isinstance(callback, ReduceLROnPlateau):
-            print("[INFO] Invoking ReduceLROnPlateau callback.")
+            logger.info("Invoking ReduceLROnPlateau callback.")
             callback(val_loss)
 
         elif isinstance(callback, EarlyStopping):
-            print("[INFO] Invoking EarlyStopping callback.")
+            logger.info("Invoking EarlyStopping callback.")
             callback(epoch + 1, val_loss)
