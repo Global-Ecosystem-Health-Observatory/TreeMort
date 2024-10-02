2bae92f4b1bf80542f92d02764c5c75df1e37b42
Anis Rahman <anisr@HP4CX34M7C.local>
2024-10-02 10:36:56 +0300

    Added Fisher matrix

--- scripts/run_fisher.sh
+++ scripts/run_fisher.sh
@@ -43,6 +43,7 @@ echo "       CPUs per task: $SLURM_CPUS_PER_TASK"
 echo "       Memory per CPU: $SLURM_MEM_PER_CPU MB"
 echo "       Job time limit: $SLURM_TIMELIMIT"
 
+echo python -m treemort.utils.fisher "$CONFIG_PATH"
 srun python -m treemort.utils.fisher "$CONFIG_PATH"
 
 EXIT_STATUS=$?
