9dfaac6fcc87e0bbbf6f7f609c8bde85a5dce0f1
Anis Rahman <anisr@HP4CX34M7C.local>
2024-10-02 12:03:32 +0300

    Fix for script

--- scripts/run_fisher.sh
+++ scripts/run_fisher.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=tree-mort
-#SBATCH --account=project_2004205
-#SBATCH --output=output/stdout/%A_%a
-#SBATCH --ntasks=1 --cpus-per-task=4
-#SBATCH --mem-per-cpu=32G
-#SBATCH --gres=gpu:v100:1
-#SBATCH --time=02:00:00
-#SBATCH --partition=gpu
-
-# Usage:
-# export VENV_PATH="/custom/path/to/venv"
-# sbatch --export=CONFIG_PATH="/custom/path/to/config" run_fisher.sh
+#SBATCH --job-name=treemort-creator                # Job name
+#SBATCH --account=project_2004205                  # Project account
+#SBATCH --output=output/stdout/%A_%a.out           # Output log
+#SBATCH --error=output/stderr/%A_%a.err            # Error log
+#SBATCH --ntasks=1                                 # Number of tasks (1 process)
+#SBATCH --cpus-per-task=6                          # Number of CPU cores per task
+#SBATCH --time=00:05:00                            # Time limit (hh:mm:ss)
+#SBATCH --partition=small                          # Partition to submit to
+#SBATCH --mem-per-cpu=6000                         # Memory per CPU in MB (6GB per CPU)
 
 MODULE_NAME="pytorch/2.3"
 
 TREEMORT_VENV_PATH="${TREEMORT_VENV_PATH:-/projappl/project_2004205/rahmanan/venv}"
 
 echo "Loading module: $MODULE_NAME"
-module load "$MODULE_NAME"
+module load $MODULE_NAME
 
 if [ -d "$TREEMORT_VENV_PATH" ]; then
     echo "[INFO] Activating virtual environment at $TREEMORT_VENV_PATH"
@@ -27,6 +24,4 @@ else
     exit 1
 fi
 
-srun python3 -c "import torch; print(\"TORCH\")"
-srun python3 -c "import treemort; print(\"TREEMORT\")"
-
+srun python -c "import treemort; print('TREEMORT')"
