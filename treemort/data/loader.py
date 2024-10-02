16cfcc31704924a2072073ddb1bcedf02a5bd29b
Anis Rahman <anisr@HP4CX34M7C.local>
2024-10-03 00:39:50 +0300

    Mixed transfer learning

--- treemort/data/loader.py
+++ treemort/data/loader.py
@@ -1,10 +1,10 @@
 import random
 
 from pathlib import Path
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 
 from treemort.data.dataset import DeadTreeDataset
-from treemort.data.sampler import BalancedSampler
+from treemort.data.sampler import DatasetAwareBalancedSampler
 from treemort.data.image_processing import get_image_processor
 
 from treemort.utils.augment import Augmentations
@@ -12,13 +12,28 @@ from treemort.utils.datautils import load_and_organize_data, stratify_images_by_
 
 
 def prepare_datasets(conf):
-    hdf5_path = Path(conf.data_folder).parent / conf.hdf5_file
 
-    image_patch_map = load_and_organize_data(hdf5_path)
+    conf.hdf5_file_finnish = 'Finland_RGBNIR_25cm.h5'
+    conf.hdf5_file_polish = 'Poland_RGBNIR_25cm.h5'
 
-    train_keys, val_keys, test_keys = stratify_images_by_patch_count(image_patch_map, conf.val_size, conf.test_size)
+    hdf5_path_finnish = Path("/scratch/project_2008436/rahmanan/dead_trees/Finland/RGBNIR/25cm").parent / conf.hdf5_file_finnish
+    hdf5_path_polish = Path("/scratch/project_2008436/rahmanan/dead_trees/Poland/RGBNIR/25cm").parent / conf.hdf5_file_polish
 
-    random.seed(None) # makes loader non-deterministic
+    #hdf5_path_finnish = Path(conf.data_folder).parent / conf.hdf5_file_finnish
+    #hdf5_path_polish = Path(conf.data_folder).parent / conf.hdf5_file_polish
+
+    image_patch_map_finnish = load_and_organize_data(hdf5_path_finnish)
+    image_patch_map_polish = load_and_organize_data(hdf5_path_polish)
+
+    train_keys_finnish, val_keys_finnish, test_keys_finnish = stratify_images_by_patch_count(
+        image_patch_map_finnish, conf.val_size, conf.test_size
+    )
+
+    train_keys_polish, val_keys_polish, test_keys_polish = stratify_images_by_patch_count(
+        image_patch_map_polish, conf.val_size, conf.test_size
+    )
+
+    random.seed(None)  # makes loader non-deterministic
 
     train_transform = Augmentations()
     val_transform = None
@@ -26,30 +41,60 @@ def prepare_datasets(conf):
 
     image_processor = get_image_processor(conf.model, conf.backbone)
 
-    train_dataset = DeadTreeDataset(
-        hdf5_file=hdf5_path,
-        keys=train_keys,
+    train_dataset_finnish = DeadTreeDataset(
+        hdf5_file=hdf5_path_finnish,
+        keys=train_keys_finnish,
         crop_size=conf.train_crop_size,
         transform=train_transform,
         image_processor=image_processor,
     )
-    val_dataset = DeadTreeDataset(
-        hdf5_file=hdf5_path,
-        keys=val_keys,
+    val_dataset_finnish = DeadTreeDataset(
+        hdf5_file=hdf5_path_finnish,
+        keys=val_keys_finnish,
         crop_size=conf.val_crop_size,
         transform=val_transform,
         image_processor=image_processor,
     )
-    test_dataset = DeadTreeDataset(
-        hdf5_file=hdf5_path,
-        keys=test_keys,
+    test_dataset_finnish = DeadTreeDataset(
+        hdf5_file=hdf5_path_finnish,
+        keys=test_keys_finnish,
         crop_size=conf.test_crop_size,
         transform=test_transform,
         image_processor=image_processor,
     )
 
-    train_loader = DataLoader(train_dataset, batch_size=conf.train_batch_size, sampler=BalancedSampler(hdf5_path, train_keys), drop_last=True)
-    val_loader = DataLoader(val_dataset, batch_size=conf.val_batch_size, sampler=BalancedSampler(hdf5_path, val_keys), shuffle=False, drop_last=True)
-    test_loader = DataLoader(test_dataset, batch_size=conf.test_batch_size, sampler=BalancedSampler(hdf5_path, test_keys), shuffle=False, drop_last=True)
-    
+    train_dataset_polish = DeadTreeDataset(
+        hdf5_file=hdf5_path_polish,
+        keys=train_keys_polish,
+        crop_size=conf.train_crop_size,
+        transform=train_transform,
+        image_processor=image_processor,
+    )
+    val_dataset_polish = DeadTreeDataset(
+        hdf5_file=hdf5_path_polish,
+        keys=val_keys_polish,
+        crop_size=conf.val_crop_size,
+        transform=val_transform,
+        image_processor=image_processor,
+    )
+    test_dataset_polish = DeadTreeDataset(
+        hdf5_file=hdf5_path_polish,
+        keys=test_keys_polish,
+        crop_size=conf.test_crop_size,
+        transform=test_transform,
+        image_processor=image_processor,
+    )
+
+    mixed_train_dataset = ConcatDataset([train_dataset_finnish, train_dataset_polish])
+    mixed_val_dataset = ConcatDataset([val_dataset_finnish, val_dataset_polish])
+    mixed_test_dataset = ConcatDataset([test_dataset_finnish, test_dataset_polish])
+
+    train_sampler = DatasetAwareBalancedSampler(hdf5_path_finnish, train_keys_finnish, hdf5_path_polish, train_keys_polish)
+    val_sampler = DatasetAwareBalancedSampler(hdf5_path_finnish, val_keys_finnish, hdf5_path_polish, val_keys_polish)
+    test_sampler = DatasetAwareBalancedSampler(hdf5_path_finnish, test_keys_finnish, hdf5_path_polish, test_keys_polish)
+
+    train_loader = DataLoader(mixed_train_dataset, batch_size=conf.train_batch_size, sampler=train_sampler, drop_last=True)
+    val_loader = DataLoader(mixed_val_dataset, batch_size=conf.val_batch_size, sampler=val_sampler, shuffle=False, drop_last=True)
+    test_loader = DataLoader(mixed_test_dataset, batch_size=conf.test_batch_size, sampler=test_sampler, shuffle=False, drop_last=True)
+
     return train_loader, val_loader, test_loader
--- treemort/data/sampler.py
+++ treemort/data/sampler.py
@@ -32,3 +32,54 @@ class BalancedSampler(Sampler):
 
     def __len__(self):
         return 2 * min(len(self.dead_tree_indices), len(self.no_dead_tree_indices))
+
+
+class DatasetAwareBalancedSampler(Sampler):
+    def __init__(self, hdf5_file_finnish, keys_finnish, hdf5_file_polish, keys_polish):
+        self.hdf5_file_finnish = hdf5_file_finnish
+        self.keys_finnish = keys_finnish
+        self.dead_tree_indices_finnish = []
+        self.no_dead_tree_indices_finnish = []
+
+        self.hdf5_file_polish = hdf5_file_polish
+        self.keys_polish = keys_polish
+        self.dead_tree_indices_polish = []
+        self.no_dead_tree_indices_polish = []
+
+        with h5py.File(self.hdf5_file_finnish, "r") as hf_finnish:
+            for idx, key in enumerate(self.keys_finnish):
+                contains_dead_tree = hf_finnish[key].attrs.get("contains_dead_tree", 0)
+                if contains_dead_tree:
+                    self.dead_tree_indices_finnish.append(idx)
+                else:
+                    self.no_dead_tree_indices_finnish.append(idx)
+
+        with h5py.File(self.hdf5_file_polish, "r") as hf_polish:
+            for idx, key in enumerate(self.keys_polish):
+                contains_dead_tree = hf_polish[key].attrs.get("contains_dead_tree", 0)
+                if contains_dead_tree:
+                    self.dead_tree_indices_polish.append(idx)
+                else:
+                    self.no_dead_tree_indices_polish.append(idx)
+
+    def __iter__(self):
+        min_dead_tree_count = min(len(self.dead_tree_indices_finnish), len(self.dead_tree_indices_polish))
+        min_no_dead_tree_count = min(len(self.no_dead_tree_indices_finnish), len(self.no_dead_tree_indices_polish))
+
+        dead_tree_sample_finnish = random.sample(self.dead_tree_indices_finnish, min_dead_tree_count)
+        dead_tree_sample_polish = random.sample(self.dead_tree_indices_polish, min_dead_tree_count)
+
+        no_dead_tree_sample_finnish = random.sample(self.no_dead_tree_indices_finnish, min_no_dead_tree_count)
+        no_dead_tree_sample_polish = random.sample(self.no_dead_tree_indices_polish, min_no_dead_tree_count)
+
+        balanced_indices_finnish = dead_tree_sample_finnish + no_dead_tree_sample_finnish
+        balanced_indices_polish = dead_tree_sample_polish + no_dead_tree_sample_polish
+
+        balanced_indices = balanced_indices_finnish + balanced_indices_polish
+        random.shuffle(balanced_indices)
+
+        return iter(balanced_indices)
+
+    def __len__(self):
+        return 2 * (min(len(self.dead_tree_indices_finnish), len(self.dead_tree_indices_polish)) +
+                    min(len(self.no_dead_tree_indices_finnish), len(self.no_dead_tree_indices_polish)))
