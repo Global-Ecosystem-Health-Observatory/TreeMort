Fixed computation of mean metrics now that metrics may output NaNs.

treemort/training/train_loop.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 from tqdm import tqdm
@@ -9,6 +10,7 @@ def train_one_epoch(model, optimizer, criterion, metrics, train_loader, conf, de
     model.train()
     train_loss = 0.0
     train_metrics = {}
+    notna_batch_counts = {}
 
     class_weights = torch.tensor(conf.class_weights, dtype=torch.float32).to(device)
     
@@ -34,12 +36,15 @@ def train_one_epoch(model, optimizer, criterion, metrics, train_loader, conf, de
         for key, value in batch_metrics.items():
             if key not in train_metrics:
                 train_metrics[key] = 0.0
-            train_metrics[key] += value.item()
+                notna_batch_counts[key] = 0
+            if not value.isnan():
+                train_metrics[key] += value.item()
+                notna_batch_counts[key] += 1
 
         train_progress_bar.set_postfix({"Train Loss": train_loss / (batch_idx + 1)})
 
     train_loss /= len(train_loader)
     for key in train_metrics:
-        train_metrics[key] /= len(train_loader)
+        train_metrics[key] /= notna_batch_counts[key] if notna_batch_counts[key] != 0 else np.nan
 
     return train_loss, train_metrics

treemort/training/validation_loop.py
@@ -1,5 +1,5 @@
+import numpy as np
 import torch
-
 from tqdm import tqdm
 
 from treemort.training.output_processing import process_model_output
@@ -9,6 +9,7 @@ def validate_one_epoch(model, criterion, metrics, val_loader, conf, device):
     model.eval()
     val_loss = 0.0
     val_metrics = {}
+    notna_batch_counts = {}
 
     class_weights = torch.tensor(conf.class_weights, dtype=torch.float32).to(device)
 
@@ -30,12 +31,15 @@ def validate_one_epoch(model, criterion, metrics, val_loader, conf, device):
             for key, value in batch_metrics.items():
                 if key not in val_metrics:
                     val_metrics[key] = 0.0
-                val_metrics[key] += value.item()
+                    notna_batch_counts[key] = 0
+                if not value.isnan():
+                    val_metrics[key] += value.item()
+                    notna_batch_counts[key] += 1
 
             val_progress_bar.set_postfix({"Val Loss": val_loss / (batch_idx + 1)})
 
     val_loss /= len(val_loader)
     for key in val_metrics:
-        val_metrics[key] /= len(val_loader)
+        val_metrics[key] /= notna_batch_counts[key] if notna_batch_counts[key] != 0 else np.nan
 
     return val_loss, val_metrics

treemort/utils/iou.py
@@ -242,13 +242,13 @@ class IOUCallback:
         adjusted_dice_scores,
         mcc_scores,
     ):
-        mean_iou_pixels = np.mean(pixel_ious)
-        mean_iou_trees = np.mean(tree_ious)
-        mean_iou = np.mean(mean_ious)
-        mean_balanced_iou = np.mean(balanced_ious)
-        mean_dice_score = np.mean(dice_scores)
-        mean_adjusted_dice_score = np.mean(adjusted_dice_scores)
-        mean_mcc = np.mean(mcc_scores)
+        mean_iou_pixels = np.nanmean(pixel_ious)
+        mean_iou_trees = np.nanmean(tree_ious)
+        mean_iou = np.nanmean(mean_ious)
+        mean_balanced_iou = np.nanmean(balanced_ious)
+        mean_dice_score = np.nanmean(dice_scores)
+        mean_adjusted_dice_score = np.nanmean(adjusted_dice_scores)
+        mean_mcc = np.nanmean(mcc_scores)
 
         return {
             "mean_iou_pixels": mean_iou_pixels,