ee18d41a336640a00d6216d892df5fdffe1674a3
Anis Rahman <anisr@HP4CX34M7C.local>
2025-04-11 17:24:13 +0500

    Fixed creator submit

--- scripts/run_creator.sh
+++ scripts/run_creator.sh
@@ -75,7 +75,7 @@ if [ -z "$SLURM_TRES_PER_TASK" ]; then
 fi
 
 # Run the Python script using the virtual environment's python3
-srun python3 "$TREEMORT_REPO_PATH/dataset/creator.py" "$DATA_CONFIG_PATH" --num-workers "$SLURM_TRES_PER_TASK"
+srun python3 "$TREEMORT_REPO_PATH/dataset/creator.py" "$DATA_CONFIG_PATH" --num-workers 6
 
 EXIT_STATUS=$?
 if [ "${EXIT_STATUS:-0}" -ne 0 ]; then
