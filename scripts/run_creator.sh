86a1867605b377336db80ca9a3a0ece97114629a
Anis Rahman <anisr@HP4CX34M7C.local>
2025-04-11 11:34:03 +0500

    Fixed creator submit

--- scripts/run_creator.sh
+++ scripts/run_creator.sh
@@ -32,6 +32,8 @@ cat <<EOT > $SBATCH_SCRIPT
 #SBATCH --partition=$PARTITION_NAME
 #SBATCH --mem-per-cpu=6000
 
+export SLURM_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK:-6}
+
 $MODULE_USE_CMD
 echo "Loading module: $MODULE_NAME"
 module load $MODULE_NAME
@@ -55,12 +57,12 @@ if [ -z "$TREEMORT_REPO_PATH" ]; then
     exit 1
 fi
 
-if [ -z "\$SLURM_CPUS_PER_TASK" ]; then
+if [ -z "$SLURM_CPUS_PER_TASK" ]; then
     echo "[WARNING] SLURM_CPUS_PER_TASK is not set. Defaulting to 1."
     SLURM_CPUS_PER_TASK=1
 fi
 
-srun python3 "$TREEMORT_REPO_PATH/dataset/creator.py" "$DATA_CONFIG_PATH" --num-workers "\$SLURM_CPUS_PER_TASK"
+srun python3 "$TREEMORT_REPO_PATH/dataset/creator.py" "$DATA_CONFIG_PATH" --num-workers "$SLURM_CPUS_PER_TASK"
 
 EXIT_STATUS=$?
 if [ "${EXIT_STATUS:-0}" -ne 0 ]; then
