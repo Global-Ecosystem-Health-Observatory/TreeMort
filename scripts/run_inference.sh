386b692863f47d6720f2120ef84a43ec608f4db4
Anis Rahman <anisr@HP4CX34M7C.local>
2025-05-03 08:44:59 +0500

    Minor - Changed time alloc

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -107,7 +107,7 @@ $ARRAY_DIRECTIVE
 #SBATCH --cpus-per-task=2
 #SBATCH --time=05:00:00
 #SBATCH --partition=$PARTITION_NAME
-#SBATCH --mem=48G
+#SBATCH --mem=96G
 $GPU_DIRECTIVE
 
 export LIST_FILE="$LIST_FILE"
