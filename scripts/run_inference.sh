21fadf32e366f5c28137def805ef9c5d5504c1f9
Anis Rahman <anisr@HP4CX34M7C.local>
2025-05-09 17:04:55 +0500

    Minor - Fixed postprocess flag

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -92,7 +92,7 @@ fi
 
 echo "[INFO] Starting inference..."
 
-echo srun python3 "$TREEMORT_REPO_PATH/inference/engine.py" "$DATA_PATH" --config "$CONFIG_PATH" --model-config "$MODEL_CONFIG_PATH" --data-config "$DATA_CONFIG_PATH" --outdir "$OUTPUT_PATH" \$POST_PROCESS
+srun python3 "$TREEMORT_REPO_PATH/inference/engine.py" "$DATA_PATH" --config "$CONFIG_PATH" --model-config "$MODEL_CONFIG_PATH" --data-config "$DATA_CONFIG_PATH" --outdir "$OUTPUT_PATH" \$POST_PROCESS
 
 EXIT_STATUS=\$?
 if [ \$EXIT_STATUS -ne 0 ]; then
