befe502832829233df261a66b96e6dd9fb639bc9
Anis Rahman <anisr@HP4CX34M7C.local>
2025-05-09 16:52:02 +0500

    Minor - Fixed postprocess flag

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -80,13 +80,13 @@ fi
 
 POST_PROCESS=""
 
-while [[ "\$#" -gt 0 ]]; do
-    if [ -z "\$1" ]; then
+while [[ "$#" -gt 0 ]]; do
+    if [ -z "$1" ]; then
         break
     fi
-    case \$1 in
+    case $1 in
         --post-process) POST_PROCESS="--post-process" ;;
-        *) echo "[ERROR] Unknown parameter passed: \$1"; exit 1 ;;
+        *) echo "[ERROR] Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
