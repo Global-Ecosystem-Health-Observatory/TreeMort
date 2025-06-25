2f5ef13f2638edf425c1fc3d427980e147d6287f
Anis Rahman <anisr@Mac.local>
2025-06-25 10:41:40 +0300

    Minor - boolean bug fix

--- treemort/training/trainer.py
+++ treemort/training/trainer.py
@@ -65,7 +65,7 @@ def trainer(
             extra_params["lambda_feature"] = conf.distillation_lambda
             extra_params["use_feature_loss"] = getattr(conf, "use_feature_loss", True)
             extra_params["use_confidence_weighting"] = str(getattr(conf, "use_confidence_weighting", "True")).lower() == "true"
-            extra_params["use_fg_bg_weighting"] = getattr(conf, "use_fg_bg_weighting", True)
+            extra_params["use_fg_bg_weighting"] = str(getattr(conf, "use_fg_bg_weighting", "True")).lower() == "true"
             extra_params["sharpen_temperature"] = getattr(conf, "distillation_sharpen_temperature", 4.0)
 
         if hasattr(conf, "distillation_sharpen_temperature"):
