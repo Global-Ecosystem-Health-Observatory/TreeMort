e085afee6ae748d1bf4c684de9441e7763f619ba
Anis Rahman <anisr@Mac.local>
2025-08-25 12:59:13 +0300

    Minor - Added best model arg

--- treemort/modeling/callback_builder.py
+++ treemort/modeling/callback_builder.py
@@ -3,7 +3,7 @@ import os
 from treemort.utils.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 
-def build_callbacks(n_batches, output_dir, optimizer, best_model_path="best.weights.pth"):
+def build_callbacks(n_batches, output_dir, optimizer, best_model="best.weights.pth"):
     checkpoint_dir = os.path.join(output_dir, "Checkpoints")
     os.makedirs(checkpoint_dir, exist_ok=True)
 
@@ -14,7 +14,7 @@ def build_callbacks(n_batches, output_dir, optimizer, best_model_path="best.weig
     )
 
     best_checkpoint_cb = ModelCheckpoint(
-        os.path.join(output_dir, best_model_path),
+        os.path.join(output_dir, best_model),
         monitor="val_loss",
         mode="min",
         save_best_only=True,
