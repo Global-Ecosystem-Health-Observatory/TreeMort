433dfa9949df645563324b552005f4e63a793219
Anis Rahman <anisr@Mac.local>
2025-11-08 22:07:56 +0200

    Prepare run treemort script for new lumi project

--- scripts/submit_treemort.sh
+++ scripts/submit_treemort.sh
@@ -19,15 +19,15 @@ export HPC_TYPE="$1"
 export MODEL_TYPE="$2"
 export DATA_TYPE="$3"
 
-export TREEMORT_REPO_PATH="/users/rahmanan/TreeMort"
+export TREEMORT_REPO_PATH="/users/aurahman/TreeMort"
 
 # Set global environment variables based on HPC type.
 if [ "$HPC_TYPE" == "puhti" ]; then
-    export TREEMORT_VENV_PATH="/projappl/project_2004205/rahmanan/venv"
-    export TREEMORT_DATA_PATH="/scratch/project_2008436/rahmanan/dead_trees"
+    export TREEMORT_VENV_PATH="/projappl/project_2004205/aurahman/venv"
+    export TREEMORT_DATA_PATH="/scratch/project_2008436/aurahman/dead_trees"
 elif [ "$HPC_TYPE" == "lumi" ]; then
-    export TREEMORT_VENV_PATH="/projappl/project_462001070/rahmanan/venv"
-    export TREEMORT_DATA_PATH="/scratch/project_462001070/rahmanan/dead_trees"
+    export TREEMORT_VENV_PATH="/projappl/project_462001070/aurahman/venv"
+    export TREEMORT_DATA_PATH="/scratch/project_462001070/aurahman/dead_trees"
 else
     echo "Error: Unsupported HPC_TYPE '$HPC_TYPE'."
     exit 1
--- treemort/main.py
+++ treemort/main.py
@@ -111,7 +111,7 @@ Usage: ./submit_treemort.sh <hpc_type> <model config file> <data config file> [-
 
 Examples:
 
-export TREEMORT_REPO_PATH="/users/rahmanan/TreeMort"
+export TREEMORT_REPO_PATH="/users/aurahman/TreeMort"
 
 (train) bash $TREEMORT_REPO_PATH/scripts/submit_treemort.sh lumi unet finland
 (test)  bash $TREEMORT_REPO_PATH/scripts/submit_treemort.sh lumi unet finland --eval-only
