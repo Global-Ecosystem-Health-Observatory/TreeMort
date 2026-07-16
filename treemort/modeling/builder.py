c8c59f9b45ef724e9f0942ca9ba91969e7b7544f
aniskhan25 <aniskhan25@gmail.com>
2026-07-16 17:15:42 +0500

    Fix - Enable find_unused_parameters for DDP to handle flair_unet auxiliary heads

--- treemort/modeling/builder.py
+++ treemort/modeling/builder.py
@@ -34,7 +34,7 @@ def resume_or_load(conf, id2label, n_batches, device, is_main=True):
         logger.info("Training model from scratch.")
 
     if dist.is_available() and dist.is_initialized():
-        model = DDP(model, device_ids=[0])
+        model = DDP(model, device_ids=[0], find_unused_parameters=True)
 
     return model, optimizer, schedular, criterion, metrics, callbacks
 
